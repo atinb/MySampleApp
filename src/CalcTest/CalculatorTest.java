6b43554b69dcad700db1299599be05d5de1370f6
atinb <atinb@microsoft.com>
2015-08-07 15:38:36 +0530

    More calc functions

--- src/CalcTest/CalculatorTest.java
+++ src/CalcTest/CalculatorTest.java
@@ -7,11 +7,37 @@ import org.junit.Test;
 public class CalculatorTest {
 
 	@Test
-	public void test() {
+	public void testadd() {
 		Calc c = new Calc();
 		int i = c.add(2,2);
 		assert(i == 2);
-				
 	}
-
+	@Test
+	public void testsubtract() {
+		Calc c = new Calc();
+		int i = c.subtract(2,2);
+		assert(i == 0);
+	}
+	@Test
+	public void testmultiply() {
+		Calc c = new Calc();
+		int i = c.multiply(2,2);
+		assert(i == 4);
+	}
+	
+	@Test
+	public void testdivide() {
+		Calc c = new Calc();
+		int i = c.divide(2,2);
+		assert(i == 1);
+	}
+	
+	@Test
+	public void testmod() {
+		Calc c = new Calc();
+		int i = c.mod(2,2);
+		assert(i == 0);
+	}
 }
+
+
--- src/Calculator/Calc.java
+++ src/Calculator/Calc.java
@@ -3,10 +3,29 @@ package Calculator;
 public class Calc {
 
 	public int add(int a , int b){
-		
+
 		return (a+b);
 	}
-	
+
+	public int subtract(int a , int b){
+
+		return (a-b);
+	}
+
+	public int multiply(int a , int b){
+
+		return (a*b);
+	}
+
+	public int divide(int a , int b){
+
+		return (a/b);
+	}
+
+	public int mod(int a , int b){
+
+		return (a%b);
+	}
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
