838aba7c047cb37e89737fcba8b9056d27c4266d
Atin Bansal <atinb@users.noreply.github.com>
2015-08-11 21:55:41 +0530

    Update CalculatorTest.java

--- src/Test/CalculatorTest.java
+++ src/Test/CalculatorTest.java
@@ -37,6 +37,24 @@ public class CalculatorTest {
 		int i = c.mod(2,2);
 		assert(i == 0);
 	}
+	
+	public void testand() {
+		Calc c = new Calc();
+		int i = c.bitand(2,2);
+		assert(i == 2);
+	}
+	
+	public void testor() {
+		Calc c = new Calc();
+		int i = c.bitor(2,2);
+		assert(i == 2);
+	}
+	
+	public void testxor() {
+		Calc c = new Calc();
+		int i = c.bitxor(2,2);
+		assert(i == 2);
+	}
 }
 
 
